8ce86641456d2a63aab4ed5537cf423bdafbc44a
Akshay Chincholkar <akshaychincholkar@gmail.com>
2023-06-14 00:48:40 +0530

    adapter dp

---
+++ src/main/java/com/game/designpatterns/structural/adapter/Volt.java
@@ -0,0 +1,18 @@
+package com.game.designpatterns.structural.adapter;
+
+public class Volt {
+    private int volts;
+
+    public int getVolts() {
+        return volts;
+    }
+
+    public void setVolts(int volts) {
+        this.volts = volts;
+    }
+
+    Volt(int volts){
+        this.volts = volts;
+    }
+
+}
