978e25b1a2e51b630bf9eb2a5545012a381fbd89
Akshay Chincholkar <akshaychincholkar@gmail.com>
2023-06-17 23:56:15 +0530

    proxy design pattern basic implementation

---
+++ src/main/java/com/game/designpatterns/structural/proxy/CommandExecutorImpl.java
@@ -0,0 +1,11 @@
+package com.game.designpatterns.structural.proxy;
+
+public class CommandExecutorImpl implements CommandExecutor{
+
+    @Override
+    public void runCommand(String command) throws Exception {
+        // some heavy implementation
+        Runtime.getRuntime().exec(command);
+        System.out.println("'"+command+"' command executed");
+    }
+}
