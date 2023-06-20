7948977452caa0a5d242f7acf78b950619cf9ece
Akshay Chincholkar <akshaychincholkar@gmail.com>
2023-06-21 00:44:09 +0530

    Fascade dp initial commit

---
+++ src/main/java/com/game/designpatterns/structural/fascade/MySqlHelper.java
@@ -0,0 +1,16 @@
+package com.game.designpatterns.structural.fascade;
+
+import java.sql.Connection;
+
+public class MySqlHelper {
+    public static Connection getMySqlDbConnection(){
+        // get the mysql connection code
+        return null;
+    }
+    public void getMySqlPdfReport(String tableName,Connection conn){
+        // code implementation
+    }
+    public void getMySqlHtmlReport(String tablename,Connection conn){
+        // code implementation
+    }
+}
---
+++ src/main/java/com/game/designpatterns/structural/fascade/OracleHelper.java
@@ -0,0 +1,16 @@
+package com.game.designpatterns.structural.fascade;
+
+import java.sql.Connection;
+
+public class OracleHelper {
+    public static Connection getOracleDbConnection(){
+        // get db connection using parameters
+        return null;
+    }
+    public void getPdfOracleReport(String tablename,Connection conn){
+        // code implementation
+    }
+    public void getHtmlOracleReport(String tableName,Connection conn){
+        // code implementation
+    }
+}
