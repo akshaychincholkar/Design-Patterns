da5ca029b4cee204f9dce2e7f3603e8b9e58934f
Akshay Chincholkar <akshaychincholkar@gmail.com>
2023-06-26 02:03:36 +0530

    Decorator initial commit

---
+++ src/main/java/com/game/designpatterns/structural/decorator/BasicCar.java
@@ -0,0 +1,8 @@
+package com.game.designpatterns.structural.decorator;
+
+public class BasicCar implements Car{
+    @Override
+    public void assemble() {
+        System.out.println("Basic car");
+    }
+}
