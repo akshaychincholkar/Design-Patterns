79f11bbfd3f93b037dcaf2cb7a3396c2eef67c6f
Akshay Chincholkar <akshaychincholkar@gmail.com>
2023-06-30 23:26:33 +0530

    mediator dp - commit 2

--- src/main/java/com/game/designpatterns/behavioral/mediator/User.java
+++ src/main/java/com/game/designpatterns/behavioral/mediator/User.java
@@ -1,4 +1,12 @@
 package com.game.designpatterns.behavioral.mediator;
 
-public class User {
+public abstract class User {
+    private ChatMediator mediator;
+    private String name;
+    User(ChatMediator mediator,String name){
+        this.mediator = mediator;
+        this.name = name;
+    }
+    public abstract  void send(String msg);
+    public abstract void receive(String msg);
 }
