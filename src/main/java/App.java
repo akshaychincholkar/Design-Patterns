2d4f6bbdcf82b0a5d36f66e31a72e4aa92644087
Darshana <patilk.darshana@gmail.com>
2024-01-13 18:49:46 +0530

    test commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,14 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
